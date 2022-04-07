0675a1af3e17c6a52ebab9f91de8761306f03fef
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-04-07 01:17:47 +0000

    save scanner

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const params = {
   host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   open: false, // When false, it won't load your browser by default.
   ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-  file: "public/index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  root: "public", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+  // file: "public/index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 500, // Waits for all changes, before reloading. Defaults to 0 sec.
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
   middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
